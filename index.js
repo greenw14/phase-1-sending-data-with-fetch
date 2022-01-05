c74777369140e037c6c445b806694791c33c59fe
william <willhgreen02@gmail.com>
2022-01-05 15:09:53 -0700

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+
+   function submitData(firstName, email) {
+       const confgObj = {
+           method: "POST",
+           headers: {
+               "Content-Type": "application/json",
+               "Accept": "application/json"
+           },
+       body: JSON.stringify({
+           "name": firstName,
+           "email": email
+       })
+    };
+    return fetch("http://localhost:3000/users" , confgObj)
+   .then(res => res.json())
+  .then(body => addNewContact(body.id))
+  
+  .catch(response => 'Unauthorized Access')
+}
+function addNewContact(newID){
+    document.getElementsByTagName("body")[0].innerHTML = newID;
+    document.body.append(message)
+}
+submitData("Name" , "name@name.com");
